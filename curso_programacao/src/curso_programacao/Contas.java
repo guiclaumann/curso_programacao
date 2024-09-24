ecf3dd6423706bb318676bdec7b13e1e433896c7
guiclaumann <guiclaumann@hotmail.com>
2024-09-24 10:47:06 -0300

    organizando dados, foi criado uma nova classe "NewScanner"

--- curso_programacao/src/curso_programacao/Contas.java
+++ curso_programacao/src/curso_programacao/Contas.java
@@ -2,76 +2,73 @@ package curso_programacao;
 
 public class Contas {
 
-	public static void main(String [] args) {
-		
+	public static void main(String[] args) {
+
 		////////////////////////
-		
+
 ///////// exemplo: 1
-		
-		//int x, y;
-		
-		//x = 5;
-		
-		//y = 2 *x;
-		
-			//System.out.println(x);
-			//System.out.println(y);
-	
-		
+
+		// int x, y;
+
+		// x = 5;
+
+		// y = 2 *x;
+
+		// System.out.println(x);
+		// System.out.println(y);
+
 		////////////////////////
-		
+
 ///////// exemplo: 2
-		
-		//int x;
-		//double y;
-		
-		//x =5;
-		
-		//y = 2 * x;
-		
-			//System.out.println(x);
-			//System.out.println(y);
-		
-		
+
+		// int x;
+		// double y;
+
+		// x =5;
+
+		// y = 2 * x;
+
+		// System.out.println(x);
+		// System.out.println(y);
+
 		////////////////////////
-		
+
 ///////// exemplo: 3
-		
-		//double b, B, h, area;
-		
-		//b = 6.0;
-		//B = 8.0;
-		//h = 5.0;
-		
-		//area = (b + B) / 2.0 * h;
-		
-			//System.out.println(area);
-		
+
+		// double b, B, h, area;
+
+		// b = 6.0;
+		// B = 8.0;
+		// h = 5.0;
+
+		// area = (b + B) / 2.0 * h;
+
+		// System.out.println(area);
+
 		///////////////////////
-			
+
 ///////// exemplo: 4
-		
-		//int a, b;
-		//double resultado;
-		
-		//a = 5;
-		//b = 2;
-		
-		//resultado = (double) a / b;
-		
-			//System.out.println(resultado);
-		
-		
+
+		// int a, b;
+		// double resultado;
+
+		// a = 5;
+		// b = 2;
+
+		// resultado = (double) a / b;
+
+		// System.out.println(resultado);
+
 		//////////////////////
-		
+
 ///////// exemplo: 5
-		//double a;
-		//int b;
-		
-		//a = 5.0;
-		//b = (int)a;
-		
-		//System.out.println(b);
-		
+		// double a;
+		// int b;
+
+		// a = 5.0;
+		// b = (int)a;
+
+		// System.out.println(b);
+
 	}
 }
--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -2,27 +2,24 @@
 package curso_programacao;
 
 import java.util.Locale;
-import java.util.Scanner;
+
 public class Main {
 
-	
 	public static void main(String[] args) {
 		int idade = 31;
 		double altura = 1.6898;
 		String nome = "maria";
 		double renda = 4000.0;
-		
-			System.out.println(idade);
-			System.out.printf("%.2f%n", altura);
-			System.out.printf("%.4f%n", altura);
-			Locale.setDefault(Locale.US);
-			System.out.printf("%.4f%n", altura);
-			System.out.println("resultado = " + altura + " METROS");
-			System.out.printf("RESULTADO = %.2f metros%n", altura);
-			System.out.printf("%s tem %d anos e ganha R$: %.2f por mês", nome, idade, renda);
-			
-			
-			
+
+		System.out.println(idade);
+		System.out.printf("%.2f%n", altura);
+		System.out.printf("%.4f%n", altura);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", altura);
+		System.out.println("resultado = " + altura + " METROS");
+		System.out.printf("RESULTADO = %.2f metros%n", altura);
+		System.out.printf("%s tem %d anos e ganha R$: %.2f por mês", nome, idade, renda);
+
 	}
 
 }
